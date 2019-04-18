b02956e579dd283ff382044145c52053e8962a69
shul3 <ssulziks@gmail.com>
2019-04-18 10:15:11 +0200

    Fix

--- www/js/product.js
+++ www/js/product.js
@@ -6,6 +6,7 @@ class Product {
       // just transfer all properties 
       // from data to "this" 
       Object.assign(this, data);
+      this.category = [];
     }
    
   }
@@ -14,12 +15,15 @@ class Product {
   let temp = require('..json/sortiment.json');
   let products = [];
   for(let t of temp){
-    products.push(new Product(t));
+      let tmp = new Product(t);
+      tmp.category.push(tmp.varugrupp);
+      tmp.category.push(tmp.varugrupp + ' från ' + tmp.ursprunglandnamn);
+    products.push(tmp);
   }
    
   // Nu är products en array med 18 695 instanser av Product
   // Export the class as a module if on backend
-  
+
 if(typeof module === 'object'){
     module.exports = Product;
   }
