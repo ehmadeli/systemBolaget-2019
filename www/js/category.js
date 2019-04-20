da7059256d946806e88c4c7fd05792c45fd91224
shul3 <ssulziks@gmail.com>
2019-04-20 16:00:53 +0200

    Fixing

--- www/js/category.js
+++ www/js/category.js
@@ -3,11 +3,9 @@ let assert = require('assert');
 class Category {
  
     constructor(data){
-      this.categories = JSON.stringify(data);
-
-        this.array = this.categories.split(",");
-        this.array[0] = this.array[0].substr(1);
-        this.array[this.array.length -1] = this.array[this.array.length -1].slice(0, -1);
+      this.array = JSON.stringify(data).split(",");
+      this.array[0] = this.array[0].substr(1);
+      this.array[this.array.length -1] = this.array[this.array.length -1].slice(0, -1);
     }
 }
 
