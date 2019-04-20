afa6e14b04aee40712c049cc4a3fa20e1e6bcf3e
shul3 <ssulziks@gmail.com>
2019-04-20 16:02:39 +0200

    Update

--- www/js/tdd/test.js
+++ www/js/tdd/test.js
@@ -13,10 +13,6 @@ let allTests = [
     new CategoryTest()
 ]
 
-
-// console.log(Object.getOwnPropertyNames(personTest));
-// console.log(Object.getOwnPropertyNames(personTest.__proto__));
-
 for (let testClass of allTests){
 let allMethods = Object.getOwnPropertyNames(testClass.__proto__);
     for (let method of allMethods){
