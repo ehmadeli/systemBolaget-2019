ee0ad18ab3983908f6513435b9ba4482873b1cdd
shul3 <ssulziks@gmail.com>
2019-05-01 13:06:55 +0200

    Updates

--- step-definitions/adjustquantity.js
+++ step-definitions/adjustquantity.js
@@ -1,86 +1,76 @@
-let Product = require('../www/js/product.js');
-let ProductCart = require('../www/js/productcart.js');
-let data = require('../www/json/sortiment.json');
+let App = require('../www/js/app.js');
 
 module.exports = function(){
 
-  let products = [];
-  let prodCart = new ProductCart();
   let prodQuantity;
+  let app = new App();
   
-  for(let t of data){
-      let tmp = new Product(t);
-      tmp.category.push(tmp.varugrupp);
-      tmp.category.push(tmp.varugrupp + ' från ' + tmp.ursprunglandnamn);
-      products.push(tmp);
-  }
-
-  prodCart.addProductToCart(products[0], 5);
-    prodQuantity = prodCart.quantity[0];
+  app.cart.addProductToCart(app.products[0], 5);
+  prodQuantity = app.cart.quantity[0];
 
   this.Given(/^the product is already in the shopping cart$/, function () {
     
-    assert(prodCart.products.length > 0, 'The product cart does not have products');
+    assert(app.cart.products.length > 0, 'The product cart does not have products');
   });
 
 
   this.When(/^i press the minus button$/, function () {
-    prodCart.changeQuantityOfProduct(products[0], prodQuantity - 1);
+    app.cart.changeQuantityOfProduct(app.products[0], prodQuantity - 1);
   });
 
   this.Then(/^it should decrease one product from the cart$/, function () {
-    assert(prodCart.quantity[0] == prodQuantity - 1, 'The products quantity from the cart is not decresed by one');
+    assert(app.cart.quantity[0] == prodQuantity - 1, 'The products quantity from the cart is not decresed by one');
   });
 
   
   this.When(/^I press the plus button$/, function () {
-    prodCart.changeQuantityOfProduct(products[0], prodQuantity + 1);
+    prodCart.changeQuantityOfProduct(app.products[0], prodQuantity + 1);
   });
 
   this.Then(/^it should increase one product into the cart$/, function () {
-    assert(prodCart.quantity[0] == prodQuantity + 1, 'The products quantity from the cart is not increased by one');
+    assert(app.cart.quantity[0] == prodQuantity + 1, 'The products quantity from the cart is not increased by one');
   });
 
   this.When(/^I increase the quantity for that product by one$/, function () {
-    prodCart.changeQuantityOfProduct(products[0], prodQuantity + 1);
+    app.cart.changeQuantityOfProduct(app.products[0], prodQuantity + 1);
   });
        
 
   this.When(/^i decrease the quantity for that product by one$/, function () {
-    prodCart.changeQuantityOfProduct(products[0], prodQuantity - 1);
+    app.cart.changeQuantityOfProduct(app.products[0], prodQuantity - 1);
   });
 
   this.When(/^i type into textbox the amount of products$/, function () {
     prodQuantity = 10;
-    prodCart.changeQuantityOfProduct(products[0], prodQuantity);
+    app.cart.changeQuantityOfProduct(app.products[0], prodQuantity);
   });
 
       
   this.Then(/^it should add as many products to the cart$/, function () {
-    assert(prodCart.quantity[0] == prodQuantity, 'The products quantity from the cart is not the same');
+    assert(app.cart.quantity[0] == prodQuantity, 'The products quantity from the cart is not the same');
   });
 
   this.When(/^i type the number of amount to zero in the shopping cart$/, function () {
     prodQuantity = 0;
-    prodCart.changeQuantityOfProduct(products[0], prodQuantity); 
+    app.cart.changeQuantityOfProduct(app.products[0], prodQuantity); 
   });
 
   this.Then(/^it should be removed from the cart$/, function () {
-       assert(!prodCart.products.includes(products[0].namn), "The product is not removed from the cart");
+       assert(!app.cart.products.includes(app.products[0]), "The product is not removed from the cart");
   });
 
   this.Given(/^that there is already one product in the shopping cart$/, function () {
-    prodCart.emptyProductCart();
-    prodCart.addProductToCart(products[0], 1);
-    prodQuantity = prodCart.quantity[0];
-    assert(prodCart.products.length == 1, 'The product cart does not have product');
+    app.cart.emptyProductCart();
+    app.cart.addProductToCart(app.products[0], 1);
+    prodQuantity = app.cart.quantity[0];
+    assert(app.cart.products.length == 1, 'The product cart does not have product');
   });
 
   this.When(/^i decrease the quantity of that product by one$/, function () {
-    prodCart.changeQuantityOfProduct(products[0], prodQuantity - 1);
+    app.cart.changeQuantityOfProduct(app.products[0], prodQuantity - 1);
   });
 
   this.Then(/^the product should be removed from the cart$/, function () {
-    assert(!prodCart.products.includes(products[0].namn), "The product is not removed from the cart");
+    assert(!app.cart.products.includes(app.products[0]), "The product is not removed from the cart");
   });
 }
--- step-definitions/emptycart.js
+++ step-definitions/emptycart.js
@@ -1,51 +1,39 @@
-let Product = require('../www/js/product.js');
-let ProductCart = require('../www/js/productcart.js');
-let data = require('../www/json/sortiment.json');
-
+let App = require('../www/js/app.js');
 
 module.exports = function(){
 
-  let products = [];
-  let prodCart = new ProductCart();
-  //let validQuantity = 3;
-
-  for(let t of data){
-      let tmp = new Product(t);
-      tmp.category.push(tmp.varugrupp);
-      tmp.category.push(tmp.varugrupp + ' från ' + tmp.ursprunglandnamn);
-      products.push(tmp);
-  }
-  
+  let app = new App();
+    
   this.Given(/^that there is already one product in the cart$/, function () {
-    prodCart.addProductToCart(products[0], 1);
-    assert(prodCart.products.length > 0, 'The product cart does not have products');
+    app.cart.addProductToCart(app.products[0], 1);
+    assert(app.cart.products.length > 0, 'The product cart does not have products');
   });
     
   this.When(/^i remove the product from the shopping cart$/, function () {        
-      let tmp = prodCart.products.length;
-      prodCart.deleteProductFromCart(products[0]);
+      let tmp = app.cart.products.length;
+      app.cart.deleteProductFromCart(app.products[0]);
 
-      assert(prodCart.products.length < tmp, "The product is not removed from cart");
+      assert(app.cart.products.length < tmp, "The product is not removed from cart");
   });
 
   this.Then(/^It should not contain any product$/, function () {
-    assert(prodCart.products.length === 0, "The product cart contaons products");
+    assert(app.cart.products.length === 0, "The product cart contaons products");
   });
 
 this.Given(/^that there are already several products in the cart$/, function() {
-    prodCart.addProductToCart(products[0], 1);
-    prodCart.addProductToCart(products[1], 3);
-    prodCart.addProductToCart(products[2], 2);
-    prodCart.addProductToCart(products[3], 3);
-    assert(prodCart.products.length > 1, 'The product cart does not have several products');
+  app.cart.addProductToCart(app.products[0], 1);
+  app.cart.addProductToCart(app.products[1], 3);
+  app.cart.addProductToCart(app.products[2], 2);
+  app.cart.addProductToCart(app.products[3], 3);
+    assert(app.cart.products.length > 1, 'The product cart does not have several products');
   });
 
  this.When(/^i remove all the products from shopping cart button$/, function () {
-        prodCart.emptyProductCart();
+  app.cart.emptyProductCart();
   });
 
 this.Then(/^It should not contain any products$/, function () {
-  assert(prodCart.products.length === 0 && prodCart.price.length === 0 && prodCart.quantity.length === 0 , 'The product cart contains anyproducts');
+  assert(app.cart.products.length === 0 && app.cart.price.length === 0 && app.cart.quantity.length === 0 , 'The product cart contains anyproducts');
 });
 
 }
