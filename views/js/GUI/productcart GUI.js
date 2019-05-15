eb199fd6a074550476678dd9eb6c253d0e53abf1
ehmad.eli <hafizaa@kth.se>
2019-05-15 23:12:26 +0200

    productcart GUI class added

--- views/js/GUI/productcart GUI.js
+++ views/js/GUI/productcart GUI.js
@@ -1,4 +1,43 @@
+class GUIproductCart {
+    constructor() {
+        this.cart = new ProductCart();
+        this.updateListOfProducts();
+        $('.emptyCart').click((e) => {
+            this.cart.empty();
+        });
+        $('.add').click((e) => {
+            this.cart.add();
+            this.updateListOfProducts();
+        });
+    }
 
+    updateListOfProducts() {
+        let rows = this.cart.overviewOfCart();
+        let html = <table class="cart-items table">
+            <thead>
+                <tr>
+                    <th scope='col'>Produckt</th>
+                    <th scope='col'>Antal</th>
+                    <th scope='col'>Summa</th>
+                    <th scope='col'></th>
+                </tr>
+            </thead>
+            <tbody>
+                for(let row of rows){
+                    html += <tr>
+                        <td>${rows.product.namn}</td>
+                        <td><input type='number' value='${row.quantity}'></input></td>
+                        <td>${row.rowSum}</td>
+                        <td><button class=' btn btn-primary'></button>Ta bort</td>
+                    </tr>
+                };
+                    }
+                    html+=
+       </tbody>
+        </table>
+            ;
+        $('.cart-items').html(html);
+    }
 
-
+}
 
