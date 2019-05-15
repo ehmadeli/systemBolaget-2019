850e0c758642546cb095f5e440e9321876830839
ehmad.eli <hafizaa@kth.se>
2019-05-15 11:08:33 +0200

    changes in step definations and in html

--- step-definitions/differentquantities.js
+++ step-definitions/differentquantities.js
@@ -6,5 +6,63 @@ let app = new App();
 let prodQuantity;
 let newProdQuantity;
   
+this.Given(/^the product is available in the shop$/, function () {
+    // Write code here that turns the phrase above into concrete actions
+ 
+  });
+ 
+  this.Given(/^I type (\d+) into text box$/, function () {
+    // Write code here that turns the phrase above into concrete actions
+
+  });
+  this.When(/^i press enter button$/, function () {
+    // Write code here that turns the phrase above into concrete actions
+
+  });
+
+  this.Then(/^it should be added (\d+) unit to the cart of the same product$/, function () {
+    // Write code here that turns the phrase above into concrete actions
+
+  });
+
+  this.Given(/^the product is available in the shop$/, function () {
+    // Write code here that turns the phrase above into concrete actions
+
+  });
+
+  this.Given(/^I type (\d+) or more units in the text box$/, function () {
+    // Write code here that turns the phrase above into concrete actions
+
+  });
+
+  this.When(/^i press enter button$/, function () {
+    // Write code here that turns the phrase above into concrete actions
+
+  });
+
+  this.Then(/^it should be added (\d+) units to the cart of the same product$/, function () {
+    // Write code here that turns the phrase above into concrete actions
+
+  });
+
+  this.Given(/^the different products are available in the shop$/, function () {
+    // Write code here that turns the phrase above into concrete actions
+
+  });
+
+  this.Given(/^I type different units into the text box for different products$/, function () {
+    // Write code here that turns the phrase above into concrete actions
+
+  });
+
+  this.When(/^i press enter button$/, function () {
+    // Write code here that turns the phrase above into concrete actions
+
+  });
+
+  this.Then(/^it should be added different units to the cart of the different products$/, function () {
+    // Write code here that turns the phrase above into concrete actions
+
+  });
 
 }
