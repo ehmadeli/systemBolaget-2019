789c165cfbe9376713deeb47fddf2910d0af8e18
ehmad.eli <hafizaa@kth.se>
2019-05-15 11:22:41 +0200

    update in step definations

--- step-definitions/OverviewOfCart-test.js
+++ step-definitions/OverviewOfCart-test.js
@@ -2,44 +2,47 @@ const App = require('../views/js/app.js');
 
 module.exports = function () {
   let app = new App();
-  
-  this.Given(/^that I already have products in the cart$/, function () {
-    // Write code here that turns the phrase above into concrete actions
 
+  this.Given(/^that I already have products in the cart$/, async function () {
+
+await helpers.loadPage('.addtocart'+ .addtocart);
   });
 
-  this.When(/^I click on cart\-button$/, function () {
-    // Write code here that turns the phrase above into concrete actions
+  this.When(/^I click on cart\-button$/, async function () {
 
 
-    this.Then(/^I can see all the products in the cart and their details$/, function () {
-      // Write code here that turns the phrase above into concrete actions
+
+    this.Then(/^I can see all the products in the cart and their details$/, async function () {
+
 
     });
 
-    this.Given(/^that I already have products in the cart$/, function () {
-      // Write code here that turns the phrase above into concrete actions
+    this.Given(/^that I already have products in the cart$/, async function () {
+
 
     });
 
-    this.When(/^I click on cart\-button$/, function () {
-      // Write code here that turns the phrase above into concrete actions
+    this.When(/^I click on cart\-button$/, async function () {
+
+      let cartButton = await $(.header.cart);
+      assert.notEqual(cartButton, null, 'could not find the cart button');
+      await cartButton.click();
 
     });
-    this.Then(/^I can see the total price of products in the cart$/, function () {
-      // Write code here that turns the phrase above into concrete actions
+    this.Then(/^I can see the total price of products in the cart$/, async  function () {
+
 
     });
-    this.Given(/^that I already have products in the cart$/, function () {
-      // Write code here that turns the phrase above into concrete actions
+    this.Given(/^that I already have products in the cart$/,async  function () {
+
 
     });
-    this.When(/^I change the quantities of the products$/, function () {
-      // Write code here that turns the phrase above into concrete actions
+    this.When(/^I change the quantities of the products$/,async  function () {
+
 
     });
-    this.Then(/^I can see new total price of products in the cart$/, function () {
-      // Write code here that turns the phrase above into concrete actions
+    this.Then(/^I can see new total price of products in the cart$/, async  function () {
+
 
     });
 
--- step-definitions/differentquantities.js
+++ step-definitions/differentquantities.js
@@ -6,61 +6,61 @@ let app = new App();
 let prodQuantity;
 let newProdQuantity;
   
-this.Given(/^the product is available in the shop$/, function () {
+this.Given(/^the product is available in the shop$/, async  function () {
     // Write code here that turns the phrase above into concrete actions
  
   });
  
-  this.Given(/^I type (\d+) into text box$/, function () {
+  this.Given(/^I type (\d+) into text box$/, async function () {
     // Write code here that turns the phrase above into concrete actions
 
   });
-  this.When(/^i press enter button$/, function () {
-    // Write code here that turns the phrase above into concrete actions
-
-  });
-
-  this.Then(/^it should be added (\d+) unit to the cart of the same product$/, function () {
+  this.When(/^i press enter button$/, async function () {
     // Write code here that turns the phrase above into concrete actions
 
   });
 
-  this.Given(/^the product is available in the shop$/, function () {
+  this.Then(/^it should be added (\d+) unit to the cart of the same product$/, async function () {
     // Write code here that turns the phrase above into concrete actions
 
   });
 
-  this.Given(/^I type (\d+) or more units in the text box$/, function () {
+  this.Given(/^the product is available in the shop$/, async function () {
     // Write code here that turns the phrase above into concrete actions
 
   });
 
-  this.When(/^i press enter button$/, function () {
+  this.Given(/^I type (\d+) or more units in the text box$/, async function () {
     // Write code here that turns the phrase above into concrete actions
 
   });
 
-  this.Then(/^it should be added (\d+) units to the cart of the same product$/, function () {
+  this.When(/^i press enter button$/, async function () {
     // Write code here that turns the phrase above into concrete actions
 
   });
 
-  this.Given(/^the different products are available in the shop$/, function () {
+  this.Then(/^it should be added (\d+) units to the cart of the same product$/, async  function () {
     // Write code here that turns the phrase above into concrete actions
 
   });
 
-  this.Given(/^I type different units into the text box for different products$/, function () {
+  this.Given(/^the different products are available in the shop$/, async function () {
     // Write code here that turns the phrase above into concrete actions
 
   });
 
-  this.When(/^i press enter button$/, function () {
+  this.Given(/^I type different units into the text box for different products$/, async function () {
     // Write code here that turns the phrase above into concrete actions
 
   });
 
-  this.Then(/^it should be added different units to the cart of the different products$/, function () {
+  this.When(/^i press enter button$/, async function () {
+    // Write code here that turns the phrase above into concrete actions
+
+  });
+
+  this.Then(/^it should be added different units to the cart of the different products$/, async function () {
     // Write code here that turns the phrase above into concrete actions
 
   });
