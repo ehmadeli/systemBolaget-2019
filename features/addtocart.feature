ea6f1e9310cdc2647fb8d9402414333d316012a9
priya panth <panthpriya12@gmail.com>
2019-05-22 16:21:37 +0200

    addcart test

--- features/addtocart.feature
+++ features/addtocart.feature
@@ -3,26 +3,26 @@ Feature: As a user, I should be able to add the selected product into the shoppi
 Scenario: Successfully add a product to the cart.
     Given that I am on the web page localhost:3306
     When I click on add-to button 
-    Then it should be added to the cart
+    Then I should see the product in the cart
 
 Scenario: Successfully add several products of the same type to the cart.
     Given that I am on the web page localhost:3306
     When I click on add-to button 3 times
-    Then it should be added to the cart
+    Then I should see the product in the cart
 
 Scenario: Successfully add a product of different types to the cart.
     Given that I am on the web page localhost:3306
     When I click on add-to button 
-    Then this different product should be added to the cart
+    Then I should see the product in the cart
 
 Scenario: Successfully add at least two different products to the cart.
     Given that I am on the web page localhost:3306
     When I click on add-to button 
     And i click again one other product's add-to cart button
-    Then the two different products should be added to the cart
+    Then I should see the product in the cart
 
 Scenario: Out of products
     Given that I am on the web page localhost:3306
     When I click to add-to button 
     Then it does not allow me to add this product to cart
-    And it shows message this product is out of stock
+    And I should see the product in the cart
--- step-definitions/addtocart-test.js
+++ step-definitions/addtocart-test.js
@@ -3,39 +3,43 @@ let { $, sleep } = require('./funcs.js');
 module.exports = function () {
 
     this.Given(/^that the products are available in the store$/, async function () {
-        await helpers.loadPage('http://localhost/%27' + portnumber + '/categories.html')
+        await helpers.loadPage('http://localhost:' + portnumber + '/categories.html')
 
     });
-
-
 
     this.When(/^I click on add\-to button$/, async function () {
-        let add = await $('.search #search');
-        let add = await $('.search .searchbutton');
+        let add = await $('.product_cart');
+        add = add[0] || add;
         await add.click();
-
-
-
+        await(1000);
     });
 
-    this.Then(/^it should be added to the cart$/, async function () {
-
+    this.Then(/^I should see the product in the cart$/, async function () {
+        this. product= await $('.product_name');
+        let cartItems = await $('.cart');
+        assert((await cartItems.getText()).includes(this.product), "The product is Auld ");
 
     });
 
     this.When(/^I click on add\-to button (\d+) times$/, async function () {
-
-
-    });
-
-    this.Then(/^it should be added to the cart$/, async function () {
-
+        let add = await $('product_cart ');
+        await add.click();
 
     });
-    this.When(/^I click on add\-to button$/, async function () {
 
-
+    this.Then(/^this different product should be added to the cart$/, async function () {
+        let cartItems = await $('.');
+        this. firstproduct= await $('.product_name');
+        assert((await cartItems[0].getText()).includes(firstproduct), "The cart do not contain 2 products");
+        this. secondproduct= await $('#productName');
+        cartItems = await $('.');
+        assert((await cartItems[1].getText()).includes(secondproduct), "The cart do not contain 2 products");
     });
+
+
+
+    
+
 
 
 
