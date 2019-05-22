01300a70821b199c94ec25a1a374fb3515e7f309
ehmad.eli <hafizaa@kth.se>
2019-05-22 11:13:06 +0200

    product cart

--- views/js/GUI/productcart GUI.js
+++ views/js/GUI/productcart GUI.js
@@ -10,33 +10,40 @@ class productCartGUI {
 
     updateListOfProducts() {
 
-        let html = `
+        let html = ` `;
 
-            
-                <ul class="cart_bar_list item_list d-flex flex-row align-items-center justify-content-end">
-                    <li class="mr-auto">Product</li>
-                    <li>Price</li>
-                    <li>Quantity</li>
-                    <li>Total</li>
-                </ul>
-
-
-    `;
 
 
         for (let x of app.cart.products) {
 
 
-            html += `<tr>
-                <td scope="row">${x.productInCart.namn}</th>
+            html += `
+            <div class="cart_items">
+            <ul class="cart_items_list">
 
-                <td >${x.productInCart.prisinklmoms}</td>
-
-                <td >${x.quantityInCart}</td>
-
-                <td >${x.priceInCart}</td>
-                
-            </tr>`;
+                <!-- Cart Item -->
+                <li class="cart_item item_list d-flex flex-lg-row flex-column align-items-lg-center align-items-start justify-content-lg-end justify-content-start">
+                    <div class="product d-flex flex-lg-row flex-column align-items-lg-center align-items-start justify-content-start mr-auto">
+                        <div><div class="product_number">1</div></div>
+                        <div><div class="product_image"><img src="images/heine.png" alt=""></div></div>
+                        <div class="product_name_container">
+                            <div class="product_name"><a href="#">${x.productInCart.namn}</a></div>
+                            <div class="product_text"></div>
+                        </div>
+                    </div>
+                    
+                    <div class="product_price product_text"><span>Price: </span>${x.productInCart.prisinklmoms}</div>
+                    <div class="product_quantity_container">
+                        <div class="product_quantity ml-lg-auto mr-lg-auto text-center">
+                            <span class="product_text product_num">${x.quantityInCart}</span>
+                            <div class="qty_sub qty_button trans_200 text-center"><span>-</span></div>
+                            <div class="qty_add qty_button trans_200 text-center"><span>+</span></div>
+                        </div>
+                    </div>
+                    <div class="product_total product_text"><span>Total: </span>${x.priceInCart}</div>
+                </li>
+            </ul>
+        </div> `;
         }
 
         $('.cart_items tbody').html(html);
