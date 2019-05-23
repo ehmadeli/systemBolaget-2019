f513749ad45bff1c4300bd557ad6f78ba8bc0c14
ehmad.eli <hafizaa@kth.se>
2019-05-23 12:52:15 +0200

    overview of cart test

--- features/overviewofcart.feature
+++ features/overviewofcart.feature
@@ -1,16 +1,14 @@
 Feature: As a User, I should be able to overview the shopping cart / its contents.
 
 Scenario: Successfully overview the shopping cart
-Given that I am on the http://localhost:3306
-And I searched for the products
-And added to the cart
+Given that I am on the page
+And I searched products and added to the cart
 When I click on cart-button to overview it
 Then I can see all the products in the cart and their details
 
 
 Scenario: Successfully overview the total price of the products
-Given that I am on the http://localhost:3306
-And I searched for the products
-And added 3 products to the cart
+Given that I am on the page
+And I searched for the products and added 3 products into the cart
 When I click on cart-button 
 Then I can see the total price of products in the cart
--- step-definitions/OverviewOfCart-test.js
+++ step-definitions/OverviewOfCart-test.js
@@ -2,44 +2,124 @@ let { $, sleep } = require('./funcs.js');
 
 module.exports = function () {
 
-this.Given(/^that I am on the http:\/\/localhost:(\d+)$/, function () {
 
-});
+  this.Given(/^that I am on the page$/, async function () {
+    await helpers.loadPage('https://localhost:3306/categories.html');
+    await sleep(5000);
+  });
 
 
-this.Given(/^I searched for the products$/, function () {
- 
-});
+  this.Given(/^I searched products and added to the cart$/, async function () {
+    let searchBox = await $('#myInput');
+ //   let searchButton = await $('.header_search_button');
 
-this.Given(/^added to the cart$/, function () {
+    await searchBox.sendKeys("Renat");
 
-});
+  //  await searchButton.click();
+    let add = await $('.product-list .product_button');
+    if (add.length > 0)
+      add = add[0];
 
-this.When(/^I click on cart\-button to overview it$/, function () {
+    assert.notEqual(add, 'could not find the add button');
+    await add.click();
+    let firstProduct = await $('.cart_item td:first-child');
+    this.addProduct = await firstProduct.getText();
+    await sleep(3000);
+
+  });
   
-});
 
-this.Then(/^I can see all the products in the cart and their details$/, function () {
 
-});
+  this.When(/^I click on cart\-button to overview it$/, async function () {
 
-this.Given(/^that I am on the http:\/\/localhost:(\d+)$/, function () {
+    let cartButton = await $('.cart');
+    assert(cartButton !== null, 'The cart does not Exist');
+    if (cartButton) {
+      await cartButton.click();
+      await sleep(3000);
+    }
+  });
 
-});
+  this.Then(/^I can see all the products in the cart and their details$/, async function () {
 
-this.Given(/^I searched for the products$/, function () {
-  
-});
+  });
 
-this.Given(/^added (\d+) products to the cart$/, function () {
-  
-});
 
-this.When(/^I click on cart\-button$/, function () {
-  
-});
 
-this.Then(/^I can see the total price of products in the cart$/, function () {
+    this.Given(/^I searched for the products and added 3 products into the cart$/, async function () {
 
-});
-}
+      let searchBox = await $('#myInput');
+   //   assert.notEqual(searchBox, null, 'could not find the search bar');
+
+      await searchBox.sendKeys('Purcari');
+   //   let searchButton = await $('.header_search_button');
+
+     // assert.notEqual(searchButton, null, 'could not find the search button');
+     // await searchButton.click();
+
+      let add = await $('.product-listing .product_button');
+      assert.notEqual(add, null, 'could not find the addbutton');
+
+      if (Array.isArray(add) == true) {
+        add = add[0];
+      }
+
+      await add.click();
+      let firstProduct = await $('.cart_item td:first-child');
+      this.addedProduct1 = await firstProduct.getText();
+
+      await searchBox.clear();
+      await searchBox.sendKeys('Canella');
+  //    await searchButton.click();
+      add = await $('.product-listing .product_button');
+
+      assert.notEqual(add, null, 'could not find the add button');
+      if (Array.isArray(add) === true) {
+        add = add[0];
+      }
+
+      await add.click();
+      let searchProduct = await $('.cart_item td:first-child');
+      this.addedProduct2 = await secondProduct[1].getText();
+
+      await searchBox.clear();
+      await searchBox.sendkeys('Svarta Trannan');
+  //    await searchButton.click();
+      add = await $('.product-listing .product_button');
+
+      assert.notEqual(add, null, 'could not find the add Button');
+      if (Array.isArray(add) === true) {
+        add = add[0];
+      }
+
+      await add.click();
+      let thirdProduct = await $('.cart_item td:first-child');
+
+      this.addedProduct3 = await thirdProduct[1].getText();
+
+    });
+
+
+    this.When(/^I click on cart\-button$/, async function () {
+
+
+      let cartButton = await $('.cart');
+      assert(cartButton !== null, 'The cart does not Exist');
+      if (cartButton) {
+        await cartButton.click();
+        await sleep(3000);
+      }
+    });
+
+    this.Then(/^I can see the total price of products in the cart$/,async function () {
+      let ProductTotalPrice = await $('.cart_item td:first-child');
+      assert((await productTotalPrice[0].getText()).includes
+        (this.addedProduct1), 'The cart do not contain 3 products');
+      productTotalPrice = await $('.cart_item td:first-child');
+
+      assert((await productTotalPrice[1].getText()).includes(this.addedProduct2), 'The cart do not contain 3 products');
+      productTotalPrice = await $('.cart_item td:first-child');
+      assert((await productTotalPrice[3].getText()).includes(this.addedProduct3), 'The cart do not contain 3 products');
+    });
+
+  }
