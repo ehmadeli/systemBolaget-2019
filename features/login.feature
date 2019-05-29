f9d8d09898e8695e873a29e47e93a0f44e6d0711
priya panth <panthpriya12@gmail.com>
2019-05-29 06:27:25 +0200

    login and singup test successfull

--- features/login.feature
+++ features/login.feature
@@ -7,5 +7,12 @@ Feature: As a User, I should be able to login my account
         When I click on sign in button
         Then it should be login Succssfully
 
+        Scenario: unSuccssfully login
+        Given that I am on the login page
+        Given I entered wrong username
+        Given I entered wrong password
+        When I click on sign in button
+        Then it should be login unSuccssfully
+
 
 
--- step-definitions/logintest.js
+++ step-definitions/logintest.js
@@ -12,14 +12,14 @@ module.exports = function(){
 this.When(/^I  entered correct username$/, async function () {
   let username = await $('#name');
   assert(username != null, 'Could not find the username');
-  await username.sendKeys('test1');
+  await username.sendKeys('priyapanth');
       
       });
     
     this.When(/^I entered correct password$/, async function () {
         let password  = await $('#pass');
         assert(password != null,'Could not find the password');
-        await password.sendKeys('test123');
+        await password.sendKeys('panth');
           });
 
       this.When(/^I click on sign in button$/, async function ()  {
@@ -35,12 +35,31 @@ this.When(/^I  entered correct username$/, async function () {
         await signin.click();
         await sleep(1000);
     });
+
+this.Given(/^I entered wrong username$/, async function () {
+  let username = await $('#name');
+  assert(username != null, 'Could not find the username');
+  await username.sendKeys('priyapanth1');
+  
+});
+this.Given(/^I entered wrong password$/, async function () {
+  let password  = await $('#pass');
+  assert(password != null,'Could not find the password');
+  await password.sendKeys('panth1');
+});
+
+    
+this.Then(/^it should be login unSuccssfully$/,async function () {
+  let login = await $('#btn');
+        assert(login != null, 'Could not find the sign button');
+        await login.click();
+        await sleep(1000);
+    });
+  
+ 
+
+
 }
-    
-    
-
-
-
 
 
 
